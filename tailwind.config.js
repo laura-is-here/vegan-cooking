3685c3b9d59db661d7f9b40eb7b3ad785d9da229
laura <lauras_here@pm.me>
2021-02-06 16:16:31 -0800

    Style adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.warmGray,
+      gray: colors.trueGray,
       red: colors.red,
       yellow: colors.amber,
       green: colors.lime,
