e2e3d59ad0ffeff5368315b280d8752bfcec0d1a
laura <lauras_here@pm.me>
2021-08-11 12:07:35 -0700

    Styles

---
+++ static/script.js
@@ -0,0 +1,8 @@
+// if (document.getElementById("recipeList")) {
+//   const recipesHeader = document.getElementById("recipes");
+//   const listToggle = document.createElement("button");
+//   const toggleLabel = document.createTextNode("View as list");
+//   listToggle.classList.add("font-sans", "text-sm", "block", "mx-auto");
+//   listToggle.appendChild(toggleLabel);
+//   recipesHeader.appendChild(listToggle);
+// }
